f0baf52adf68a2fe6435f87cf1990ea2e84ce0b1
Dalton Turner <dcturner555@gmail.com>
2020-11-17 17:23:42 -0700

    Fixed bug that caused a crash while training

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 from simpletransformers.classification import ClassificationModel
 import pandas as pd
 
+
 # https://towardsdatascience.com/simple-transformers-introducing-the-easiest-bert-roberta-xlnet-and-xlm-library-58bf8c59b2a3
 
-train_df = pd.read_csv('data/train.csv', header=None).head(10)
-eval_df = pd.read_csv('data/test.csv', header=None).head(10)
+def main():
+    train_df = pd.read_csv('data/train.csv', header=None).iloc[1:]
+    eval_df = pd.read_csv('data/test.csv', header=None).iloc[1:]
 
-train_df = pd.DataFrame({
-    'text': train_df[1].replace(r'\n', ' ', regex=True),
-    'label': train_df[2]
-})
+    # 0 = human
+    # 1 = bot
+    train_df[2] = (train_df[2] == 'bot').astype(int)
+    eval_df[2] = (eval_df[2] == 'bot').astype(int)
 
-eval_df = pd.DataFrame({
-    'text': eval_df[1].replace(r'\n', ' ', regex=True),
-    'label': eval_df[2]
-})
+    train_df = pd.DataFrame({
+        'text': train_df[1].replace(r'\n', ' ', regex=True),
+        'label': train_df[2]
+    })
+    eval_df = pd.DataFrame({
+        'text': eval_df[1].replace(r'\n', ' ', regex=True),
+        'label': eval_df[2]
+    })
 
-print(train_df.head())
+    print(train_df.head())
 
-model = ClassificationModel('roberta', 'roberta-base', use_cuda=False)
-model.train_model(train_df)
+    model = ClassificationModel('roberta', 'roberta-base', use_cuda=False)
+    model.train_model(train_df)
 
-result, model_outputs, wrong_predictions = model.eval_model(eval_df)
+    result, model_outputs, wrong_predictions = model.eval_model(eval_df)
+
+
+if __name__ == '__main__':
+    main()
